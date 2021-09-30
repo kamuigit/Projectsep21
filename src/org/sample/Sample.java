6094c458621f20891b197e09b03bcf14d718f62f
kamuigit <vishalsriram3@gmail.com>
2021-09-30 18:37:42 +0530

    br2 work done

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Sample {
+	private void brrrt() {
+		System.out.println("boring");
+	}
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
