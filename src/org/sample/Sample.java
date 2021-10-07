e031e896d56c61a7221a45899a1e872fa939b8fd
kamuigit <vishalsriram3@gmail.com>
2021-10-07 20:03:11 +0530

    tqask comp

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class Sample {
+	private void mre() {
+		System.out.println("blahblah....");
+	}
 	public static void main(String[] args) {
 		System.out.println("main method");
 	}
